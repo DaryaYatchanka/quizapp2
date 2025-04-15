c369c8904426ae8f130bc77da9e6791734dec3e9
Darya Yatchanka <darya.yatchanka@gmail.com>
2025-04-14 22:43:37 -0400

    Question first idea started

--- src/main/java/com/quizapp2/model/Question.java
+++ src/main/java/com/quizapp2/model/Question.java
@@ -1,2 +1,33 @@
-package com.quizapp2.model;public class Question {
+package com.quizapp2.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String topic;
+
+    private String questionText;
+
+    private String imageUrl;
+
+    private String questionType;
+
+    private Integer points;
+
+    //TO do : think about points, Data Model
+
 }
