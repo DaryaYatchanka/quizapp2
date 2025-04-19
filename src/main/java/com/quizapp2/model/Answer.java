6b136a9b3eafdf76b3a398ebe14c026bf71b1124
Darya Yatchanka <darya.yatchanka@gmail.com>
2025-04-19 15:06:47 -0400

    Answer entity started with To Do added to the comments

---
+++ src/main/java/com/quizapp2/model/Answer.java
@@ -0,0 +1,27 @@
+package com.quizapp2.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Answer {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+
+    /*  To Do:
+         1.Come up with necessary fields ->
+         2. DM relationships with Answer;
+ */
+
+}
